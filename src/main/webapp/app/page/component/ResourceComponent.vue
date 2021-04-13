b7f221cdc337c87af73ba02e3a30d77a6d51d5f3
Yevhen Chypachenko <Yevhen_Chypachenko@epam.com>
2021-04-13 19:33:36 +0300

    add logic for pause/resume/archive resource

--- src/main/webapp/app/page/component/ResourceComponent.vue
+++ src/main/webapp/app/page/component/ResourceComponent.vue
@@ -2,6 +2,7 @@
   <div class="col">
     <div class="card my-1">
       <div class="card-body">
+        <div v-if="error" class="alert alert-danger">Ошибка... {{ error }}</div>
         <h5 class="card-title">{{ resource.name }}
           <span v-if="resource.status === 'ACTIVE'" class="badge badge-pill badge-primary float-right">Активно</span>
           <span v-if="resource.status === 'SUSPENDED'" class="badge badge-pill badge-warning float-right">Призупинено</span>
@@ -21,10 +22,29 @@
             <td>{{ resource.created }}</td>
           </tr>
         </table>
-        <button class="btn btn-sm btn-primary"><font-awesome-icon icon="question-circle"/> Детальніше</button>
-        <button v-if="resource.status === 'ACTIVE'" class="btn btn-sm btn-warning"><font-awesome-icon icon="pause"/> Призупинити</button>
-        <button v-if="resource.status === 'SUSPENDED'" class="btn btn-sm btn-success"><font-awesome-icon icon="play"/> Поновити</button>
-        <button v-if="resource.status === 'ARCHIVED'" class="btn btn-sm btn-danger" v-on:click="removeResource()"><font-awesome-icon icon="trash"/> Видалити</button>
+        <button class="btn btn-sm btn-primary">
+          <font-awesome-icon icon="question-circle"/>
+          Детальніше
+        </button>
+
+        <button v-on:click="pauseResource()" v-if="resource.status === 'ACTIVE'" class="btn btn-sm btn-warning">
+          <font-awesome-icon icon="pause"/>
+          Призупинити
+        </button>
+
+        <button v-on:click="resumeResource()" v-if="resource.status === 'SUSPENDED'" class="btn btn-sm btn-success">
+          <font-awesome-icon icon="play"/>
+          Поновити
+        </button>
+        <button v-on:click="archiveResource()" v-if="resource.status === 'SUSPENDED'" class="btn btn-sm btn-danger">
+          <font-awesome-icon icon="archive"/>
+          Архівувати
+        </button>
+
+        <button v-on:click="removeResource()" v-if="resource.status === 'ARCHIVED'" class="btn btn-sm btn-danger">
+          <font-awesome-icon icon="trash"/>
+          Видалити
+        </button>
       </div>
     </div>
   </div>
@@ -41,6 +61,31 @@ export default class ResourceComponent extends Vue {
   public resource: IAccountResource
   @Prop()
   public resources: IAccountResource[];
+  public error: string = null;
+
+  public pauseResource(): void {
+    axios.patch(`/api/resource/${this.resource.id}`, {
+      'status': 'SUSPENDED'
+    }).then(r => {
+      this.resource.status = 'SUSPENDED';
+    }).catch(err => this.error = err);
+  }
+
+  public resumeResource(): void {
+    axios.patch(`/api/resource/${this.resource.id}`, {
+      'status': 'ACTIVE'
+    }).then(r => {
+      this.resource.status = 'ACTIVE';
+    }).catch(err => this.error = err);
+  }
+
+  public archiveResource(): void {
+    axios.patch(`/api/resource/${this.resource.id}`, {
+      'status': 'ARCHIVED'
+    }).then(r => {
+      this.resource.status = 'ARCHIVED';
+    }).catch(err => this.error = err);
+  }
 
   public removeResource(): void {
     axios.delete(`/api/resource/${this.resource.id}`)
@@ -49,7 +94,7 @@ export default class ResourceComponent extends Vue {
           if (index > -1) {
             this.resources.splice(index, 1);
           }
-        })
+        }).catch(err => this.error = err);
   }
 }
 </script>
