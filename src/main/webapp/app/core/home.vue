6698390f775d52740551c5c653991277dd9e7ad3
Yevhen Chypachenko <Yevhen_Chypachenko@epam.com>
2021-04-24 16:40:30 +0300

    update main page description

--- src/main/webapp/app/core/home.vue
+++ src/main/webapp/app/core/home.vue
@@ -1,20 +1,49 @@
 <template>
   <div>
     <div class="row py-4">
+      <div class="col-md-4 text-center">
+        <h3>Моніторінг ресурсів</h3>
+        <font-awesome-icon icon="chart-bar" size="8x"/>
+        <p>Додавайте ресурси та оримуйте інформацію про їх стан та доступність</p>
+      </div>
+      <div class="col-md-4 text-center">
+        <h3>Продвинута статистика</h3>
+        <font-awesome-icon icon="clipboard-list" size="8x"/>
+        <p>Отримуйте детальну інформацію про кожен випадок ресурсу</p>
+      </div>
+      <div class="col-md-4 text-center">
+        <h3>Завжди на зв"язку</h3>
+        <font-awesome-icon icon="comment-dots" size="8x"/>
+        <p>
+          Отримуйте інфомрацію про стан Вашого ресурсу прямо на телефон, або E-Mail.
+        </p>
+      </div>
+    </div>
+    <hr>
+    <div class="row py-4">
       <div class="col-md-4">
         <h3>Моніторінг ресурсів</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.</p>
       </div>
       <div class="col-md-4">
         <h3>Продвинута статистика</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
         </p>
       </div>
       <div class="col-md-4">
         <h3>Завжди на зв"язку</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
         </p>
       </div>
     </div>
